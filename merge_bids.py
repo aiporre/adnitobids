479006a86aee8e0b019cd6ce6758775c71c479a8
aiporre <ariel.iporre.rivas@gmail.com>
2025-04-27 16:15:17 +0200

    merge the participants tsv

--- merge_bids.py
+++ merge_bids.py
@@ -4,6 +4,7 @@ import shutil
 import argparse
 import json
 import logging
+import pandas as pd
 from pathlib import Path
 
 # Set up logging
@@ -97,6 +98,8 @@ def merge_bids_directories(source_dir, dest_dir, conflict_strategy="skip", dry_r
     # First handle the dataset description
     if not dry_run:
         merge_dataset_description(source_dir, dest_dir)
+    if not dry_run:
+        merge_particitipants_tsv(source_dir, dest_dir)
 
     # Handle subject directories and other BIDS folders
     for item in os.listdir(source_dir):
@@ -105,6 +108,8 @@ def merge_bids_directories(source_dir, dest_dir, conflict_strategy="skip", dry_r
 
         # Skip dataset_description.json as it's handled separately
         if item == "dataset_description.json":
+            continue
+        if item == "participants.tsv":
             continue
 
         # Handle directories (subjects, derivatives, code, etc.)
@@ -140,6 +145,32 @@ def merge_bids_directories(source_dir, dest_dir, conflict_strategy="skip", dry_r
                 copy_file(source_path, dest_path, conflict_strategy)
 
     return True
+
+def merge_particitipants_tsv(source_dir, dest_dir):
+    """Merge participants.tsv files."""
+    source_participants_path = os.path.join(source_dir, "participants.tsv")
+    dest_participants_path = os.path.join(dest_dir, "participants.tsv")
+
+    if not os.path.exists(source_participants_path):
+        logger.warning(f"Source participants.tsv does not exist: {source_participants_path}")
+        return
+
+    if not os.path.exists(dest_participants_path):
+        df1 = pd.read_csv(source_participants_path, sep="\t")
+        df2 = pd.read_csv(dest_participants_path, sep="\t")
+        a = pd.merge(df1, df2, on="participant_id", how='outer', suffixes=("","_drop"))
+        for col_name in a.columns:
+            if col_name.endswith("_drop") and col_name != "participant_id":
+                a[col_name] = a[col_name].fillna(a[col_name + "_drop"])
+        a = a.drop(columns=[col for col in a.columns if col.endswith("_drop")])
+        a.to_csv(dest_participants_path, sep="\t", index=False)
+        # report overlaping participants
+        logger.info(f"Merged participants.tsv files: {source_participants_path} -> {dest_participants_path}")
+        # compute overlapping participants
+        count = len(set(df1.participant_id).intersection(set(df1.participant_id)))
+        logger.info(f"Overlapping participants: {count} form source = {len(df1)} and dest={len(df2)}")
+    else:
+        logger.warning(f"Destination participants.tsv already exists: {dest_participants_path}")
 
 def main():
     args = parse_args()
